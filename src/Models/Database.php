86065d29daa318a3973fb13ee3aaad4e3936092f
JeremyPanaiva <jeremy.panaiva@etu.univ-amu.fr>
2025-10-24 15:31:29 +0200

    database error

--- src/Models/Database.php
+++ src/Models/Database.php
@@ -1,28 +1,31 @@
 <?php
 namespace Models;
 
+use Shared\Exceptions\DataBaseException;
+
 class Database {
     private static $conn = null;
 
     public static function getConnection() {
         if (self::$conn === null) {
-
             $servername = self::parseEnvVar("DB_HOST");
             $username = self::parseEnvVar("DB_USER");
             $password = self::parseEnvVar("DB_PASSWORD");
             $dbname = self::parseEnvVar("DB_NAME");
 
-
-            self::$conn = new \mysqli($servername, $username, $password, $dbname);
-
-            if (self::$conn->connect_error) {
-                die("Connexion échouée : " . self::$conn->connect_error);
+            try {
+                mysqli_report(MYSQLI_REPORT_STRICT); // toutes les erreurs deviennent exceptions
+                self::$conn = new \mysqli($servername, $username, $password, $dbname);
+                self::$conn->set_charset('utf8mb4');
+            } catch (\mysqli_sql_exception $e) {
+                // On lance notre exception personnalisée
+                throw new DataBaseException("Unable to connect to the database");
             }
         }
         return self::$conn;
     }
-    static function parseEnvVar(string $envVar)
-    {
+
+    static function parseEnvVar(string $envVar) {
         $val = getenv($envVar);
         if ($val !== false && $val !== '') {
             return $val;
@@ -37,17 +40,15 @@ class Database {
         $env = [];
         foreach ($lines as $line) {
             $line = trim($line);
-            if ($line === '' || $line[0] === '#' || $line[0] === ';') {
-                continue;
-            }
-            if (strpos($line, '=') === false) {
-                continue;
-            }
+            if ($line === '' || $line[0] === '#' || $line[0] === ';') continue;
+            if (strpos($line, '=') === false) continue;
+
             [$key, $value] = explode('=', $line, 2);
             $key = trim($key);
             $value = trim($value);
 
-            if ((str_starts_with($value, '"') && str_ends_with($value, '"')) || (str_starts_with($value, "'") && str_ends_with($value, "'"))) {
+            if ((str_starts_with($value, '"') && str_ends_with($value, '"')) ||
+                (str_starts_with($value, "'") && str_ends_with($value, "'"))) {
                 $value = substr($value, 1, -1);
             }
             $env[$key] = $value;
