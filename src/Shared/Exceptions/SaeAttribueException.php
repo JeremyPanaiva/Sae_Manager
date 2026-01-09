2961e20dd9822b850e36b1d0515e6c042bd788a0
Mohamed_D <mohamed.driouchi@etu.univ-amu.fr>
2026-01-09 17:00:16 +0100

    correction shared

--- src/Shared/Exceptions/SaeAttribueException.php
+++ src/Shared/Exceptions/SaeAttribueException.php
@@ -23,6 +23,7 @@ class SaeAttribueException extends \RuntimeException
      */
     public function __construct(string $saeTitre)
     {
-        parent::__construct("Impossible de supprimer la SAE « $saeTitre » : elle a déjà été attribuée à un ou plusieurs étudiant(s).");
+        parent::__construct("Impossible de supprimer la SAE « $saeTitre » : elle a déjà été attribuée à un ou 
+        plusieurs étudiant(s).");
     }
 }
