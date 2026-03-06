6c9abf1fbb4d65aa806d5056481dc3a2f47678a0
Mohamed <mohamed.driouchi@etu.univ-amu.fr>
2026-03-06 09:28:17 +0100

    renforcement sécurité mdp

--- src/Views/User/reset-password.php
+++ src/Views/User/reset-password.php
@@ -31,9 +31,9 @@
 
             <label for="password">Nouveau mot de passe :</label>
             <div class="password-wrapper">
-                <input type="password" id="password" name="password" minlength="8" maxlength="20"
-                    pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}" title="Le mot de passe doit contenir au moins 8
-                    caractères,une majuscule, une minuscule et un chiffre" required
+                <input type="password" id="password" name="password" minlength="12" maxlength="30"
+                    pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{12,}" title="Le mot de passe doit contenir entre 12 et 30
+                    caractères, une majuscule, une minuscule, un chiffre et un caractère spécial" required
                     placeholder="Votre nouveau mot de passe">
                 <span class="toggle-password" aria-label="Afficher/masquer le mot de passe">
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none"
@@ -44,12 +44,12 @@
                 </span>
             </div>
             <small style="display:block; margin-bottom:15px; color:#666; font-size:0.9em;">
-                Minimum 8 caractères avec au moins une majuscule, une minuscule et un chiffre
+                Minimum 12 caractères avec au moins une majuscule, une minuscule, un chiffre et un caractère spécial
             </small>
 
             <label for="confirm_password">Confirmer le mot de passe :</label>
             <div class="password-wrapper">
-                <input type="password" id="confirm_password" name="confirm_password" minlength="8" maxlength="20"
+                <input type="password" id="confirm_password" name="confirm_password" minlength="12" maxlength="30"
                     required placeholder="Confirmez votre nouveau mot de passe">
                 <span class="toggle-password" aria-label="Afficher/masquer le mot de passe">
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none"
